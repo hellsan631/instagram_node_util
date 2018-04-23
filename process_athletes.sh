601350549f2fa175d8ca0164f95302fcedfcb287
tvconsluting <tvconsulting@gmail.com>
2018-04-22 22:27:11 -0400

    Added support for getting accounts from real names

---
+++ process_athletes.sh
@@ -0,0 +1,6 @@
+# node instagram_node_util --customimport nfl_2017_salaries.csv --outputdir datanfl
+node instagram_node_util --accountnames --customimport ./athlete_data/NBA.csv --outputdir datanba
+node instagram_node_util --accountnames --customimport ./athlete_data/MLB.csv --outputdir datamlb
+node instagram_node_util --accountnames --customimport ./athlete_data/MLS.csv --outputdir datamls
+node instagram_node_util --accountnames --customimport ./athlete_data/NHL.csv --outputdir datanhl
+node instagram_node_util --accountnames --customimport ./athlete_data/rugby.csv --outputdir datarugby
