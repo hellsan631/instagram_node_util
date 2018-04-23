c46895421915adbf5c5d37c798d5be20dc41161c
tvconsluting <tvconsulting@gmail.com>
2018-04-23 01:12:24 -0400

    Fixes/updates to accountname grabber

--- process_athletes.sh
+++ process_athletes.sh
@@ -3,9 +3,9 @@
 #node instagram_node_util --accountnames --customimport ./athlete_data/MLB.csv --outputdir datamlb
 #node instagram_node_util --accountnames --customimport ./athlete_data/MLS.csv --outputdir datamls
 #node instagram_node_util --accountnames --customimport ./athlete_data/NHL.csv --outputdir datanhl
-#node instagram_node_util --accountnames --customimport ./athlete_data/rugby.csv --outputdir datarugby
+node instagram_node_util --write_frequency=50 --accountnames --customimport ./athlete_data/rugby.csv --outputdir datarugby
 # to process:
-node instagram_node_util --accountnames --customimport ./athlete_data/tennis_men.csv --outputdir data_tennis_men
+node instagram_node_util --write_frequency=50 --accountnames --customimport ./athlete_data/tennis_men.csv --outputdir data_tennis_men
 node instagram_node_util --accountnames --customimport ./athlete_data/tennis_women.csv --outputdir data_tennis_women
 node instagram_node_util --accountnames --customimport ./athlete_data/pga.csv --outputdir data_pga
 node instagram_node_util --accountnames --customimport ./athlete_data/fifa.csv --outputdir data_fifa
